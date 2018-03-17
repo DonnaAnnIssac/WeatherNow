41534782637e69ce3b4a4d40fb327495c1907fd2
Donna Ann Issac <donna.ann1209@gmail.com>
2018-03-17 20:15:12 +0530

    First commit

---
+++ App.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput
+} from 'react-native'
+import {getByCity} from './call_api'
+import Forecast from './Forecast.js'
+
+export default class WeatherNow extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      zip: null,
+      forecast: {
+        main: null,
+        description: null,
+        temp: null,
+        pressure: null,
+        humidity: null,
+        windspeed: null,
+        sunrise: null,
+        sunset: null,
+        city: null
+      }
+    }
+  }
+  handleInputChange (event) {
+    let city = event.nativeEvent.text
+    getByCity(city, this)
+  }
+  render () {
+    return (
+      <View style={styles.container}>
+        <View style={styles.zipContainer}>
+          <Text style={styles.welcomeText}>
+            Where are you now?
+          </Text>
+          <View style={styles.inputBox}>
+            <TextInput
+              underlineColorAndroid={'transparent'}
+              style={styles.cityName}
+              onSubmitEditing={(event) => this.handleInputChange(event)}
+              placeholderTextColor='#FFFFFF'
+              placeholder='Enter city here' />
+          </View>
+        </View>
+        {/* if (this.state.forecast.main !== null) { */}
+          <Forecast {...this.state} />
+        {/* } */}
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 30,
+    height: '100%',
+    width: '100%',
+    backgroundColor: '#000051'
+  },
+  zipContainer: {
+    flex: 1,
+    borderBottomColor: '#DDDDDD',
+    borderBottomWidth: 1,
+    display: 'flex',
+    marginLeft: 0,
+    marginTop: 3,
+    marginBottom: 10
+  },
+  welcomeText: {
+    flex: 1,
+    fontSize: 20,
+    color: '#FFFFFF'
+  },
+  inputBox: {
+    flex: 2,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  cityName: {
+    flex: 1,
+    width: '60%',
+    fontSize: 16,
+    color: '#FFFFFF'
+  }
+})
---
+++ components/Forecast.js
@@ -0,0 +1,73 @@
+import React, {Component} from 'react'
+import {
+    StyleSheet,
+    Text,
+    View
+} from 'react-native'
+import Moment from 'moment-timezone'
+
+export default class extends Component {
+  appendZeroes (time) {
+    return (time.toString().length === 1) ? '0' + time : time
+  }
+  convertToLocalTime (ts, city) {
+    // let date = new Date(ts * 1000)
+    // let hours = (date.getHours())
+    // let minutes = date.getMinutes()
+    // let seconds = date.getSeconds()
+    // return (this.appendZeroes(hours) + ' : ' + this.appendZeroes(minutes) + ' : ' + this.appendZeroes(seconds))
+    return Moment().tz(ts, city)
+  }
+  render () {
+    return (
+      <View style={styles.outputBox}>
+        <Text style={styles.bigText}>
+          {this.props.main}
+        </Text>
+        <Text style={styles.bigText}>
+          {this.props.description}
+        </Text>
+        <Text style={styles.bigText}>
+          Temperature: {this.props.temp} K
+        </Text>
+        <Text style={styles.bigText}>
+          Windspeed: {this.props.windspeed} m/s
+        </Text>
+        <Text style={styles.bigText}>
+          Pressure: {this.props.pressure} hPa
+        </Text>
+        <Text style={styles.bigText}>
+          Humidity: {this.props.humidity} %
+        </Text>
+        <Text style={styles.mainText}>
+          {this.convertToLocalTime(this.props.sunrise, this.props.city)}
+        </Text>
+        <Text style={styles.mainText}>
+          {this.convertToLocalTime(this.props.sunset, this.props.city)}
+        </Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  outputBox: {
+    flex: 3,
+    display: 'flex',
+    marginTop: 20,
+    marginBottom: 10
+  },
+  bigText: {
+    flex: 1,
+    fontSize: 20,
+    textAlign: 'center',
+    // margin: 10,
+    color: '#FFFFFF'
+  },
+  mainText: {
+    flex: 1,
+    fontSize: 16,
+    textAlign: 'center',
+    color: '#FFFFFF'
+  }
+})
