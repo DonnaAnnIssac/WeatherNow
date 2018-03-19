f9d5558578618ec08fe0b51b532d306f4b153425
Donna Ann Issac <donna.ann1209@gmail.com>
2018-03-19 14:00:52 +0530

    Add weather icon to view

--- components/Forecast.js
+++ components/Forecast.js
@@ -2,21 +2,22 @@ import React, {Component} from 'react'
 import {
     StyleSheet,
     Text,
-    View
+    View,
+    Image
 } from 'react-native'
-import Moment from 'moment-timezone'
+// import Moment from 'moment-timezone'
 
 export default class extends Component {
   appendZeroes (time) {
     return (time.toString().length === 1) ? '0' + time : time
   }
   convertToLocalTime (ts, city) {
-    // let date = new Date(ts * 1000)
-    // let hours = (date.getHours())
-    // let minutes = date.getMinutes()
-    // let seconds = date.getSeconds()
-    // return (this.appendZeroes(hours) + ' : ' + this.appendZeroes(minutes) + ' : ' + this.appendZeroes(seconds))
-    return Moment().tz(ts, city)
+    let date = new Date(ts * 1000)
+    let hours = (date.getHours())
+    let minutes = date.getMinutes()
+    let seconds = date.getSeconds()
+    return (this.appendZeroes(hours) + ' : ' + this.appendZeroes(minutes) + ' : ' + this.appendZeroes(seconds))
+    // return Moment().tz(ts, city)
   }
   render () {
     return (
@@ -24,6 +25,7 @@ export default class extends Component {
         <Text style={styles.bigText}>
           {this.props.main}
         </Text>
+        <Image style={styles.weatherIcon} source={{uri: 'http://openweathermap.org/img/w/' + this.props.icon + '.png'}} />
         <Text style={styles.bigText}>
           {this.props.description}
         </Text>
@@ -61,7 +63,6 @@ const styles = StyleSheet.create({
     flex: 1,
     fontSize: 20,
     textAlign: 'center',
-    // margin: 10,
     color: '#FFFFFF'
   },
   mainText: {
@@ -69,5 +70,10 @@ const styles = StyleSheet.create({
     fontSize: 16,
     textAlign: 'center',
     color: '#FFFFFF'
+  },
+  weatherIcon: {
+    width: 50,
+    height: 50,
+    alignSelf: 'center'
   }
 })
